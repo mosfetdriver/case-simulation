2f5fd3c1c3566ee82c5e9b78efa1b74c86add021
christian-leufulab <99040088+mosfetdriver@users.noreply.github.com>
2023-09-06 15:11:41 -0300

    Added optimization file

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -1,6 +1,11 @@
 import powercomponents
-import cplex
 import pandas as pd
 
-profiles = pd.read_excel('C:/Users/cmsan/OneDrive/Documents/GitHub/case-simulation/simulation/data.xlsx')
+#CONSTANTS
+cable_ampacity        = 115                                       # [A]
+single_phase_voltage  = 230                                       # [V]
+three_phase_voltage   = 380                                       # [V]
+informatics_max_power = 3 * single_phase_voltage * cable_ampacity # [W]
+electricity_price     = 150                                       # [CLP/kWh]
 
+profiles = pd.read_excel('C:/Users/cmsan/OneDrive/Documents/GitHub/case-simulation/simulation/data.xlsx')
